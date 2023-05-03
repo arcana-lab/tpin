fa9e86e5f50fdf19c9b9316c2a28772d4e5bd797
fsossai <federico.sossai@gmail.com>
2023-05-02 20:50:35 -0500

    Removed `tpin.sh`

--- tpin.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-tpin_dir="$(dirname "$0")"
-
-nthreads_per_core=$(lscpu | grep "Thread(s) per core" | egrep -o "[0-9]+")
-nsockets=$(lscpu | grep "Socket(s)" | egrep -o "[0-9]+")
-
-$tpin_dir/tpin.out $nthreads_per_core $nsockets
